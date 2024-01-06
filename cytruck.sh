cd52e79aff223c557b6d5cb8e32b6b9f59742ed4
ElyasMens <133586599+ElyasMens@users.noreply.github.com>
2024-01-06 14:45:04 -0500

    Update cytruck.sh
    Ajout du graphique pour -l

--- cytruck.sh
+++ cytruck.sh
@@ -51,7 +51,7 @@ function check_temp_and_images {
 	then
 		#echo "tmpdir crée"
 		mkdir temp
-	elif [ -f temp/* ]
+	elif [ "-f temp/*" ]
 	then	
 		#echo "supprime les tmp"
 		rm -f temp/*
@@ -95,18 +95,20 @@ if [ $help -eq 1 ]
 then
 	echo "Pour executer le programme: bash script [nom_fichier] [option]"
 	echo " "
+	echo "option -h: Afficher les commandes du programmes [disponible]"
 	echo "option -d1: 10 premiers conducteurs [disponible]"
 	echo "option -d2: 10 plus grandes distances [disponible]"
 	echo "option -l: 10 trajets les plus longs [disponible]"
 	echo "option -t: 10 villes les plus traversées [indisponible]"
 	echo "option -s: statistiques sur les étapes [indisponible]"
-	echo "option -h: Afficher les commandes du programmes [disponible]"
 	
 fi
 
+#Tri -d1
 if [ $trid1 -eq 1 ]
 then	
 	check_temp_and_images 
+	echo > tmp.csv | mv tmp.csv temp
 	# 1er et 2e awk: Recuperation des colones 1 et 6 et Elimination des doublons 
 	# 3e awk: Calcul du nombre de route par conducteur puis tri par ordre décroissant
 	tail -$nbLines $data | awk '{
@@ -124,15 +126,15 @@ then
 				for (driver in nbRoutes) {
 				 	print driver";"nbRoutes[driver]   
 				 }
-			}' | sort -t';' -k2 -n -r | head -10 > tmp.txt 
-			mv tmp.txt temp
-	echo "Driver;Nb Routes"
-	cat temp/tmp.txt
+			}' | sort -t';' -k2 -n -r | head -10 > temp/tmp.csv 
+	cat temp/tmp.csv
 fi
 
+#Tri -d2
 if [ $trid2 -eq 1 ]
 then
 	check_temp_and_images 
+	echo > tmp.csv | mv tmp.csv temp
 	#awk: Somme des distance en fonction de chaque conducteur puis tri par ordre décroissant
 	tail -$nbLines $data| awk -F';' '{
 		distance[$6]=distance[$6]+$5;
@@ -140,16 +142,15 @@ then
 			for ( driver in distance ) {
 				print driver";"distance[driver]
 			}
-		}' | sort -t';' -k2 -n -r | head -10 > tmp.txt 
-		 mv tmp.txt temp
-	echo "Driver;Distance"	
-	cat temp/tmp.txt
-
+		}' | sort -t';' -k2 -n -r | head -10 > temp/tmp.csv 	
+	cat temp/tmp.csv
 fi
 
+#Tri -l
 if [ $triL -eq 1 ]
 then 
 	check_temp_and_images 
+	echo > tmp.csv | mv tmp.csv temp
 	#awk: Somme des distance en fonction de chaque route puis tri par ordre décroissant 
 	tail -$nbLines $data | awk -F';' '{ 
 		distance[$1]=distance[$1]+$5;
@@ -157,22 +158,39 @@ then
 		for ( routeId in distance ) {
 			print routeId";"distance[routeId]
 		}
-	}' | sort -t';' -k2 -n -r | head -10 | sort -t';' -k1 -n -r > tmp.txt 
-	mv tmp.txt temp
-	echo "Route Id;Distance"
-	cat temp/tmp.txt
+	}' | sort -t';' -k2 -n -r | head -10 | sort -t';' -k1 -n -r > temp/tmp.csv  
+	#Avec chatgpt
+	# Crée le script Gnuplot 
+	echo "set terminal png size 900,800" > tmp_gnuplot.gp
+	echo "set output 'Img_l.png'" >> tmp_gnuplot.gp
 
-	
+	echo "set style data histogram" >> tmp_gnuplot.gp
+	echo "set style fill solid" >> tmp_gnuplot.gp
+	echo "set boxwidth 0.5" >> tmp_gnuplot.gp
 
+	echo "set xlabel 'Route ID'" >> tmp_gnuplot.gp
+	echo "set ylabel 'Distance (en km)'" >>tmp_gnuplot.gp
+	echo "set title 'Nombre de routes'" >> tmp_gnuplot.gp
+	echo "unset key" >> tmp_gnuplot.gp
+
+	echo "set datafile separator ';'" >> tmp_gnuplot.gp
+	echo "plot 'temp/tmp.csv' using 2:xtic(1) with boxes" >> tmp_gnuplot.gp
+
+
+	# Exécute Gnuplot avec le script généré
+	gnuplot -persist -c "tmp_gnuplot.gp"
+	mv Img_l.png images
 fi
 
+#Tri -t
 if [ $triT -eq 1 ]
 then
-	check_temp_and_images 
-	
+	check_temp_and_images
+	echo > tmp.csv | mv tmp.csv temp 
 	
 fi
 
+#Tri -s
 if [ $triS -eq 1 ]
 then
 	check_temp_and_images 
@@ -181,5 +199,10 @@ then
 
 fi
 
+if [ "-f temp/*" ]
+then	
+	#echo "supprime les tmp"
+	rm -f temp/*
+fi
 echo "END"
 
