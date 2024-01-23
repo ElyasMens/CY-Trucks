8b0fb266b92a00a78e67d1f0c1fc0cb4fd32038c
ElyasMens <133586599+ElyasMens@users.noreply.github.com>
2024-01-22 19:33:28 -0500

    Update Header.h

--- progc/Header.h
+++ progc/Header.h
@@ -1,7 +1,3 @@
-/*Les fichiers d’entête, ou headers. Ce sont les fichiers *.h. ils
-permettent de rassembler les “entêtes” communs à plusieurs
-fichiers sources et/ou autres fichiers d’entête.*/
-
 #ifndef HEADER_H
 #define HEADER_H
 #endif
